810995a8abadc140e0c3f66b55317350edad7039
Khiyuan <Fan.Khiyuan@gmail.com>
2015-04-02 10:25:40 +0800

    files forgot to add

---
+++ demo/KFCollectionViewAutoSizeLayout-demo/Pods/Headers/Private/KFCollectionViewLayout/KFCollectionViewAutoSizeSquareLayout.h
@@ -0,0 +1,1 @@
+../../../../../../KFCollectionViewLayout/KFCollectionViewAutoSizeLayout/KFCollectionViewAutoSizeSquareLayout.h
---
+++ demo/KFCollectionViewAutoSizeLayout-demo/Pods/Target Support Files/Pods-KFCollectionViewAutoSizeLayout-demo-KFCollectionViewLayout/Pods-KFCollectionViewAutoSizeLayout-demo-KFCollectionViewLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KFCollectionViewAutoSizeLayout_demo_KFCollectionViewLayout : NSObject
+@end
+@implementation PodsDummy_Pods_KFCollectionViewAutoSizeLayout_demo_KFCollectionViewLayout
+@end
